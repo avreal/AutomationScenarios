4618fce5b04b5bf6b32d0257bd17972c96c6bfb4
avreal <avreal31@gmail.com>
2020-06-18 16:46:37 -0700

    minor changes to print output and readme again

--- sc1-VerifyIntMac/getintmac.py
+++ sc1-VerifyIntMac/getintmac.py
@@ -1,20 +1,7 @@
 """
 Alan Villarreal - 2020
 
-Scenario: Suppose a customer has a large number of locations with us. They called in and created a ticket with support because
-they want to ensure that the arp table on the POS (trust) network at all of their locations has only one device (POS server). If a location
-has more than 1 device, they want the full arp output of those locations so they can investigate on their end (comparing MACs). 
-There are hundreds of locations and doing this manually would be tedious and time consuming. Develop a script to check that the locations 
-have only one device. They want to ensure that an attacker has not inserted a switch into the network and added their own device to 
-the pos environment.
-
-Ideally this customer would have the trust network locked down such that the specified host is the only thing allowed,
-however they have not provided this information and had us configure a /24 despite only needing 1 address.
-
-I am assuming that we already have an inventory file maintained for this customer, otherwise me might take a spreadsheet mapping 
-location codes to loopbacks (how we access these devices over our VPN) and dynamically create an inventory.
-
-In this case, the trust network is on Ethernet 0/3. We can ignore the gateway.
+See readme for scenario explanation
 
 """
 
@@ -31,37 +18,36 @@ result = nr.run(task=napalm_get,getters=["get_arp_table"])
 #print_result(result)
 
 #create a list of entries specifying not to include the gateway IP and only checking interface Ethernet0/3
-newlist = []
+arp_list = []
 for x in result:
     for k in result[x].result['get_arp_table']:
         if (k['interface'] == "Ethernet0/3") and (k['ip'] != "192.168.30.1"):
-            newlist.append({x:k})
+            arp_list.append({x:k})
 
 # This function will go through the result and create a list with the violations pertaining to having more than 1 MAC on the interface
-totalEntries = len(newlist)
-numViolationCount = 0
-numViolationLocations = 0
-violationList = []
-newLocation = True
-listlist = []
-for i in range(totalEntries):
+total_entries = len(arp_list)
+num_violation_count = 0
+num_violation_locations = 0
+violation_list = []
+new_location_flag = True
+for i in range(total_entries):
     if i == 0:
         continue
-    elif newlist[i].keys() == newlist[i-1].keys() and newLocation == True:
-        violationList.append(newlist[i])
-        violationList.append(newlist[i-1])
-        numViolationCount += 2
-        numViolationLocations += 1
-        newLocation = False
-    elif newlist[i].keys() == newlist[i-1].keys() and newLocation == False:
-        violationList.append(newlist[i])
-        numViolationCount += 1
-    elif newlist[i].keys() != newlist[i-1].keys():
-        newLocation = True
+    elif arp_list[i].keys() == arp_list[i-1].keys() and new_location_flag == True:
+        violation_list.append(arp_list[i])
+        violation_list.append(arp_list[i-1])
+        num_violation_count += 2
+        num_violation_locations += 1
+        new_location_flag = False
+    elif arp_list[i].keys() == arp_list[i-1].keys() and new_location_flag == False:
+        violation_list.append(arp_list[i])
+        num_violation_count += 1
+    elif arp_list[i].keys() != arp_list[i-1].keys():
+        new_location_flag = True
 
-print("We have found violations of the # of mac addresses policy on Ethernet 0/3 across", numViolationLocations, "locations. The full arp output for all offending locations will be printed below")
+print("We have found violations of the # of mac addresses policy on Ethernet 0/3 across", num_violation_locations, "locations. The full arp output of Ethernet 0/3 for all offending locations will be printed below")
 
-for i in violationList:
+for i in violation_list:
     print(i)
 
 
--- sc1-VerifyIntMac/readme.md
+++ sc1-VerifyIntMac/readme.md
@@ -4,18 +4,25 @@ write output to spreadsheet by location code
 
 Alan Villarreal - 2020
 
-Scenario: Suppose a customer has a large number of locations with us. They called in and created a ticket with support because they want to ensure that the arp table on the POS (trust) network at all of their locations has only one device (POS server). If a location has more than 1 device, they want the full arp output of those locations so they can investigate on their end (comparing MACs). There are hundreds of locations and doing this manually would be tedious and time consuming. Develop a script to check that the locations have only one device. They want to ensure that an attacker has not inserted a switch into the network and added their own device to the pos environment.
+Scenario: Suppose a customer has a large number of locations with us. They called in and created a ticket with support because they want to ensure that the arp table on the POS (trust) network at all of their locations has only one device (the POS server). If a location has more than 1 device, they want the full arp output of those locations so they can investigate on their end, perhaps by comparing MAC addresses to their list. There are hundreds of locations and doing this manually would be tedious and time consuming. Develop a script to check that the locations have only one device. They want to ensure that an attacker has not inserted a switch into the network and added their own device to the pos environment.
 
 Ideally this customer would have the trust network locked down such that the specified host is the only thing allowed, however they have not provided this information and had us configure a /24 despite only needing 1 address.
 
-I am assuming that we already have an inventory file maintained for this customer, otherwise me might take a spreadsheet mapping location codes to loopbacks (how we access these devices over our VPN) and dynamically create an inventory.
+I am assuming that we already have an inventory file maintained for this customer, otherwise we might take a spreadsheet mapping location codes to loopbacks (how we access these devices over our VPN) and dynamically create an inventory file.
 
 In this case, the trust network is on Ethernet 0/3. We can ignore the gateway.
 
 INPUT:
 
-This is sanitized output of the AggregatedResult object returned by nornir
+A list of dictionaries built from the output of the nornir AggregatedResult object.
+
 ~~~
+This is sanitized output of the AggregatedResult object returned by nornir
+>>> for x in result:
+...     print(x)
+...     for k in result[x].result['get_arp_table']:
+...             print(k)
+
 ABC-001-0001
 {'interface': 'Ethernet0/0', 'mac': '0C:13:B8:43:CC:01', 'ip': '192.168.10.1', 'age': 218.0}
 {'interface': 'Ethernet0/0', 'mac': 'AA:BB:CC:00:01:00', 'ip': '192.168.10.2', 'age': 0.0}
@@ -52,8 +59,10 @@ ABC-001-0004
 ~~~
 Output:
 
+A list containing dictionaries which make up the arp table of locations with mac address violations.
+
 ~~~
-We have found violations of the # of mac addresses policy on Ethernet 0/3 across 2 locations. The full arp output for all offending locations will be printed below
+We have found violations of the # of mac addresses policy on Ethernet 0/3 across 2 locations. The full arp output of Ethernet 0/3 for all offending locations will be printed below
 {'ABC-001-0001': {'interface': 'Ethernet0/3', 'mac': '00:50:79:66:68:04', 'ip': '192.168.30.20', 'age': 210.0}}
 {'ABC-001-0001': {'interface': 'Ethernet0/3', 'mac': '00:50:79:66:68:00', 'ip': '192.168.30.2', 'age': 108.0}}
 {'ABC-001-0001': {'interface': 'Ethernet0/3', 'mac': '00:50:79:66:68:05', 'ip': '192.168.30.21', 'age': 155.0}}
